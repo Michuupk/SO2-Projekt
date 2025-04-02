de49b4e1953a76cc6f091a64c153c17a6f1a2a82
Michuupk <michal.pakula3@gmail.com>
2025-04-02 21:33:41 +0200

    done

--- main.cpp
+++ main.cpp
@@ -3,44 +3,83 @@
 #include <atomic>
 #include <chrono>
 #include <random>
+#include <ncurses.h>
+#include <mutex>
 
 using namespace std;
 
 const int N = 5;
-atomic<bool> forks[N]; // true - zajęty, false - wolny
+atomic<bool> forks[N];
+atomic<bool> stop_threads(false);
+    
+mutex ncurses_mutex;
 
 random_device rd;
 mt19937 gen(rd());
-uniform_int_distribution<int> dist(2, 4);
+uniform_int_distribution<int> dist1(1, 3);
 
+random_device rd2;
+mt19937 gen2(rd2());
+uniform_int_distribution<int> dist2(5, 7);
+
+const char *Names[] = {"Yoda", "Sheldon", "Hagrid", "Peter", "Dr House"};
+
+void displayState(WINDOW *win, const char *name, int color_pair, int id)
+{
+    lock_guard<mutex> lock(ncurses_mutex);
+    wattron(win, COLOR_PAIR(color_pair));
+    mvwprintw(win, id, 1, "%s", name);
+    wattroff(win, COLOR_PAIR(color_pair));
+    wrefresh(win);
+}
 void philosopher1(int id)
 {
     int left = id;
     int right = (id + 1) % N;
 
-    while (true)
+    while (!stop_threads)
     {
+        // Think
+        displayState(stdscr, Names[id], 1, id);
+        this_thread::sleep_for(chrono::seconds(dist1(gen)));
+        bool got_left = false;
+        bool got_right = false;
 
-        forks[left] = true;
-
-        if (!forks[right])
+        // Pick up
+        while (!stop_threads)
         {
-            forks[right] = true;
+            {
+                lock_guard<mutex> lock(ncurses_mutex);
+                if (forks[left])
+                {
+                    forks[left] = false;
+                    got_left = true;
+                }
+                if (forks[right])
+                {
+                    forks[right] = false;
+                    got_right = true;
+                }
+            }
 
-            cout << "Filozof " << id << " je." << endl;
-            this_thread::sleep_for(std::chrono::seconds(dist(gen)));
+            if (got_left && got_right)
+            {
+                // Eat
+                displayState(stdscr, Names[id], 3, id);
+                this_thread::sleep_for(chrono::seconds(dist2(gen)));
 
-            // Odłożenie
-            forks[left] = false;
-            forks[right] = false;
-
-            cout << "Filozof " << id << " mysli." << endl;
-            ;
-            this_thread::sleep_for(std::chrono::seconds(dist(gen)));
-        }
-        if (forks[right])
-        {
-            //cout << "Filozof " << id << " czeka na prawa." << endl;
+                // Put down
+                lock_guard<mutex> lock(ncurses_mutex);
+                forks[left] = true;
+                forks[right] = true;
+                break;
+            }
+            else
+            {
+                // Wait
+                displayState(stdscr, Names[id], 2, id);
+                this_thread::sleep_for(chrono::milliseconds(10));
+            }
         }
     }
 }
@@ -50,73 +89,201 @@ void philosopher2(int id)
     int left = id;
     int right = (id + 1) % N;
 
-    while (true)
+    while (!stop_threads)
     {
+        // Think
+        displayState(stdscr, Names[id], 1, id);
+        this_thread::sleep_for(chrono::seconds(dist1(gen)));
+        bool got_left = false;
+        bool got_right = false;
 
-        forks[left] = true;
-
-        if (!forks[right])
+        // Pick up
+        while (!stop_threads)
         {
-            forks[right] = true;
+            {
+                lock_guard<mutex> lock(ncurses_mutex);
+                if (forks[left])
+                {
+                    forks[left] = false;
+                    got_left = true;
+                }
+                if (forks[right])
+                {
+                    forks[right] = false;
+                    got_right = true;
+                }
+            }
 
-            cout << "Filozof " << id << " je." << endl;
-            this_thread::sleep_for(std::chrono::seconds(dist(gen)));
+            if (got_left && got_right)
+            {
+                // Eat
+                displayState(stdscr, Names[id], 3, id);
+                this_thread::sleep_for(chrono::seconds(dist2(gen)));
 
-            // Odłożenie
-            forks[left] = false;
-            forks[right] = false;
-
-            cout << "Filozof " << id << " mysli." << endl;
-            ;
-            this_thread::sleep_for(std::chrono::seconds(dist(gen)));
+                // Put down
+                lock_guard<mutex> lock(ncurses_mutex);
+                forks[left] = true;
+                forks[right] = true;
+                break;
+            }
+            else
+            {
+                // Wait
+                displayState(stdscr, Names[id], 2, id);
+                this_thread::sleep_for(chrono::milliseconds(10));
+                if (id == 0)
+                {
+                    {
+                        lock_guard<mutex> lock(ncurses_mutex);
+                        got_left = false;
+                        forks[left] = true;
+                    }
+                }
+            }
         }
-        if (forks[right])
+    }
+}
+
+void philosopher3(int id)
+{
+    int left = id;
+    int right = (id + 1) % N;
+
+    while (!stop_threads)
+    {
+        // Think
+        displayState(stdscr, Names[id], 1, id);
+        this_thread::sleep_for(chrono::seconds(dist1(gen)));
+
+        bool got_left = false;
+        bool got_right = false;
+
+        // Pick up
+        while (!stop_threads)
         {
-            cout << "Filozof " << id << " czeka na prawa." << endl;
+            if (id % 2 == 0) // Even philosopher
+            {
+                {
+                    lock_guard<mutex> lock(ncurses_mutex);
+                    if (forks[left])
+                    {
+                        forks[left] = false;
+                        got_left = true;
+                    }
+                    if (forks[right] && got_left)
+                    {
+                        forks[right] = false;
+                        got_right = true;
+                    }
+                }
+            }
+            else // Odd philosopher
+            {
+                {
+                    lock_guard<mutex> lock(ncurses_mutex);
+                    if (forks[right])
+                    {
+                        forks[right] = false;
+                        got_right = true;
+                    }
+                    if (forks[left] && got_right)
+                    {
+                        forks[left] = false;
+                        got_left = true;
+                    }
+                }
+            }
+
+            if (got_left && got_right)
+            {
+                // Eating
+                displayState(stdscr, Names[id], 3, id);
+                this_thread::sleep_for(chrono::seconds(dist2(gen)));
+
+                // Put down
+                lock_guard<mutex> lock(ncurses_mutex);
+                forks[left] = true;
+                forks[right] = true;
+                got_left = false;
+                got_right = false;
+                break;
+            }
+            else
+            {
+                // Waiting
+                displayState(stdscr, Names[id], 2, id);
+                if (got_left)
+                    got_left = false;
+                    forks[left] = true;
+                if (got_right)
+                    got_right = false;
+                    forks[right] = true;
+                this_thread::sleep_for(chrono::milliseconds(10));
+            }
         }
     }
 }
 
 int main()
 {
-
     thread philosophers[N];
-    int what;
 
-    cout << "Wybierz strategie: " << endl;
-    cout << "1. Z mozliwoscia zakleszczenia." << endl;
-    cout << "2. Z zagladzaniem." << endl;
-    cout << "3. Optymalne." << endl;
-    cin >> what;
+    initscr();
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_BLACK);    // thinking
+    init_pair(2, COLOR_YELLOW, COLOR_BLACK); // waiting
+    init_pair(3, COLOR_GREEN, COLOR_BLACK);  // eating
+
+    printw("Wybierz strategie: \n");
+    printw("1. Z mozliwoscia zakleszczenia.\n");
+    printw("2. Z zagladzaniem.\n");
+    printw("3. Optymalne.\n");
+    refresh();
+
+    int what;
+    scanw("%d", &what);
+
+    clear();
+    refresh();
+
+    for (int i = 0; i < N; ++i)
+        forks[i] = true;
 
     switch (what)
     {
-    case 1:
-        for (int i = 0; i < N; ++i)
-        {
-            forks[i] = false;
-            philosophers[i] = thread(philosopher1, i);
-        }
-        for (auto &p : philosophers)
-        {
-            p.join();
-        }
-        break;
-    case 2:
-        for (int i = 0; i < N; ++i)
-        {
-            forks[i] = false;
-            philosophers[i] = thread(philosopher2, i);
-        }
-        for (auto &p : philosophers)
-        {
-            p.join();
-        }
-        break;
+        case 1:
+            for (int i = 0; i < N; ++i)
+                philosophers[i] = thread(philosopher1, i);
 
-    default:
-        cout << "Nie ma takiej opcji." << endl;
-        break;
+            getch();
+
+            break;
+            
+        case 2:
+            for (int i = 0; i < N; ++i)
+                philosophers[i] = thread(philosopher2, i);
+            getch();
+
+            break;
+        
+        case 3:
+            for (int i = 0; i < N; ++i)
+                philosophers[i] = thread(philosopher3, i);
+            getch();
+
+            break;
+
+        default:
+            break;
     }
+
+    stop_threads = true;
+
+    for (auto &p : philosophers)
+        if (p.joinable())
+            p.join();
+
+    endwin();
+
     return 0;
 }
